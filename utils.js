60b82b7ca4a6b26eb9e16fdd2787ad903ebc0138
bsspirit <bsspirit@163.com>
2014-02-17 14:49:06 +0800

    'bug_for_num'

--- utils.js
+++ utils.js
@@ -1,12 +1,14 @@
 'use strict';
 
-var Job = require('./job');
-var Time = require('./time');
+var Job = require('./job')
+    ,Time = require('./time')
+    ,Num = require('./num');
 
 var Utils = function () {
     var self = this;
     self.job = new Job();//任务管理 job tools
     self.time = new Time();//时间管理
+    self.num = new Num();//计算工具
 }
 
 module.exports = Utils;
